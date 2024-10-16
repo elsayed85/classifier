7b1720127435fc89d9eb4f38fc47b8a390d7330e
elsayed kamal <sayed.kamal@waseet.net>
2024-10-16 22:44:21 +0300

    “wip”

--- classifier_api.py
+++ classifier_api.py
@@ -34,9 +34,10 @@ def initialize_classifier():
     try:
         classifier = pipeline(
             "zero-shot-classification",
-            model="facebook/bart-large-mnli",
-            framework="pt",
-            batch_size=4,
+            # model="facebook/bart-large-mnli",
+            model="joeddav/xlm-roberta-large-xnli",
+            # framework="pt",
+            # batch_size=4,
         )
         print("Classifier initialized successfully")
     except Exception as e:
